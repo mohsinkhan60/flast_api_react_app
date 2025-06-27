81d38af7ff67fc3b31de6bba5c9b313c466f0adf
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-06-28 02:32:14 +0500

    feat: set up SQLite database and register Tortoise ORM with FastAPI

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class Product(Model):
    quantity_in_stock = fields.IntField(default=0)
    quantity_sold = fields.IntField(default=0)
    unit_price = fields.DecimalField(max_digits=8, decimal_places=2, default=0.00)
-   supplied_by = fields.ForeignKeyField('models.supplier', related_name='goods_supplier')
+   supplied_by = fields.ForeignKeyField('models.Supplier', related_name='goods_supplier')
    revenue = fields.DecimalField(max_digits=20, decimal_places=2, default=0.00)
 
 class Supplier(Model):
