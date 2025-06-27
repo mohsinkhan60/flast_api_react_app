5bfb9bf008f44f75e5a5d29e9952fbf43cb79d59
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-06-28 02:02:38 +0500

    refactor: rename Products class to Product and clean up commented code

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from tortoise.models import Model
 from tortoise import fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 
-class Products(Model):
+class Product(Model):
    id = fields.IntField(pk=True)
    name = fields.CharField(max_length=30, nullable=False)
    quantity_in_stock = fields.IntField(default=0)
@@ -10,12 +11,16 @@ class Products(Model):
    supplied_by = fields.ForeignKeyField('models.supplier', related_name='goods_supplier')
    revenue = fields.DecimalField(max_digits=20, decimal_places=2, default=0.00)
 
-# class Supplier(Model):
-#    id = fields.IntField(pk=True)
-#    name = fields.CharField(max_length=30, nullable=False)
-#    contact_info = fields.CharField(max_length=100, nullable=True)
-#    address = fields.CharField(max_length=100, nullable=True)
-#    products = fields.ReverseRelation['Products']
+class Supplier(Model):
+   id = fields.IntField(pk=True)
+   name = fields.CharField(max_length=30)
+   company = fields.CharField(max_length=30)
+   email = fields.CharField(max_length=50)
+   phone = fields.CharField(max_length=15)
    
-#    def __str__(self):
-#        return self.name
+# create pydantic models
+product_pydantic = pydantic_model_creator(Product, name='Product')
+product_pydanticIn = pydantic_model_creator(Product, name='ProductIn', exclude_readonly=True)
+
+supplier_pydantic = pydantic_model_creator(Supplier, name='Supplier')
+supplier_pydanticIn = pydantic_model_creator(Supplier, name='SupplierIn', exclude_readonly=True)
