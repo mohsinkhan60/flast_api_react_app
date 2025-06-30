8508f2189e094cad8a6a7ab6a25f0e5d6b797043
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-06-30 23:11:18 +0500

    feat: implement ProductContext for managing product state

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,15 +1,16 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
+import { BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { ProductProvider } from "./ProductContext";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          {/* Your <Route> components go here */}
-        </Routes>
+        <ProductProvider>
+          <NavBar />
+          <Routes>{/* Your <Route> components go here */}</Routes>
+        </ProductProvider>
       </BrowserRouter>
     </div>
   );
--- front_end/src/components/NavBar.js
+++ front_end/src/components/NavBar.js
@@ -1,33 +1,51 @@
-import { Badge, Button, Form, FormControl, Nav, Navbar, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import {
+  Badge,
+  Button,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  Container,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { ProductContext } from "../ProductContext";
 
-const NavBar = () => (
-  <Navbar bg="dark" expand="lg" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">Inventory Management App</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbar-nav" />
-      <Navbar.Collapse id="navbar-nav">
-        <Nav className="me-auto">
-          <Badge bg="primary" className="mt-2">Products In Stock</Badge>
-        </Nav>
+const NavBar = () => {
+   const [products, setProducts] = useContext(ProductContext)
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Inventory Management App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Badge bg="primary" className="mt-2">
+              Products In Stock {products.data.length}
+            </Badge>
+          </Nav>
 
-        <Form className="d-flex align-items-center">
-          <Link to="/addproduct" className="btn btn-primary btn-sm me-3 w-100">
-            Add Product
-          </Link>
-          <FormControl
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button type="submit" variant="outline-primary">
-            Search
-          </Button>
-        </Form>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+          <Form className="d-flex align-items-center">
+            <Link
+              to="/addproduct"
+              className="btn btn-primary btn-sm me-3 w-100"
+            >
+              Add Product
+            </Link>
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button type="submit" variant="outline-primary">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBar;
