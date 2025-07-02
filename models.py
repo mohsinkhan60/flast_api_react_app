c8c7e311caa20823ef7e720465e448e958b2cfe8
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-07-03 00:25:47 +0500

    fix: change unit_price and revenue fields to IntField in Product model

--- models.py
+++ models.py
@@ -7,8 +7,8 @@ class Product(Model):
    name = fields.CharField(max_length=30, nullable=False)
    quantity_in_stock = fields.IntField(default=0)
    quantity_sold = fields.IntField(default=0)
-   unit_price = fields.DecimalField(max_digits=8, decimal_places=2, default=0)
-   revenue = fields.DecimalField(max_digits=20, decimal_places=2, default=0)
+   unit_price = fields.IntField(max_digits=8, default=0)
+   revenue = fields.IntField(max_digits=20, default=0)
    
    supplied_by = fields.ForeignKeyField('models.Supplier', related_name='goods_supplier') 
 
